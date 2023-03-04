54c8dd43c69d333cea2cff6bb6a4b4b055a59cb0
BobrovskaOlena <lena69114@gmail.com>
2023-03-04 18:10:29 +0200

    all metods

--- MyHashMap.java
+++ MyHashMap.java
@@ -1,2 +1,12 @@
 public class MyHashMap {
+    //додає пару ключ + значення:
+    void put(Object value, Object key){}
+    //видаляє пару за ключем:
+    void remove(Object key){} 
+    //очищає колекцію:
+    void clear(){}
+    //повертає розмір колекції:
+    void size(){}
+    //повертає значення (Object value) за ключем:
+    void get(Object key){}
 }
--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,2 +1,12 @@
 public class MyLinkedList {
+   //додає елемент в кінець:
+    void add(Object value){}
+    //видаляє елемент із вказаним індексом:
+    void remove(int index){}
+    //очищає колекцію:
+    void clear(){}
+    //повертає розмір колекції:
+    void size(){}
+    //повертає елемент за індексом:
+    void get(int index){}
 }
