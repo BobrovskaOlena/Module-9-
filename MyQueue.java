1324adce930d62d4dfb1a846189c7611110667f0
BobrovskaOlena <lena69114@gmail.com>
2023-03-05 02:52:09 +0200

    3

--- MyQueue.java
+++ MyQueue.java
@@ -1,8 +1,25 @@
-public class MyQueue {
+public class MyQueue<T> {
+    private static final int numberOfBuyers = 7;
     private Object[] buyers;
-    //додає елемент в кінець:
-    void add(Object value){
+    private int num;
 
+    public MyQueue(){
+        buyers = new Object[numberOfBuyers];
+    }
+    //додає елемент в кінець:
+    public void add(T value){
+        ifNeedNewSize();
+        buyers[num]=value;
+        num++;
+    }
+    private void ifNeedNewSize(){
+        if(num==buyers.length){
+            System.out.println("Process is successful, num: "+ num + ", buyers.length is: " + buyers.length);
+            int newNum = (buyers.length*3)/2;
+            Object[] newBuyers = new Object[newNum];
+            System.arraycopy(buyers, 0, newBuyers, 0, buyers.length);
+            buyers=newBuyers;
+        }
     }
     //очищає колекцію:
     void clear(){}
--- MyQueueTest.java
+++ MyQueueTest.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+
 public class MyQueueTest {
     public static void main(String[] args) {
+        MyQueue<String> names = new MyQueue<>();
+        names.add("Olena");
+        names.add("Anton");
+        names.add("Katya");
 
     }
 }
