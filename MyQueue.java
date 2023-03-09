c8ba83fe46f809259cbe9c0589b71559cc698e9a
BobrovskaOlena <lena69114@gmail.com>
2023-03-09 17:19:42 +0200

    q

--- MyQueue.java
+++ MyQueue.java
@@ -2,57 +2,54 @@ import java.util.Arrays;
 import java.util.StringJoiner;
 
 public class MyQueue<T> {
-    private static final int numberOfBuyers = 7;
-    private Object[] buyers = new Object[numberOfBuyers];
+    private static final int number = 7;
+    private Object[] arr = new Object[number];
     private int num;
     //додає елемент в кінець:
     public void add(T value){
         ifNeedNewSize();
-        buyers[num]=value;
+        arr[num]=value;
         num++;
     }
     private void ifNeedNewSize(){
-        if(num==buyers.length){
-            System.out.println("Process is successful, num: "+ num + ", buyers.length is: " + buyers.length);
-            int newNum = (buyers.length*3)/2;
+        if(num==arr.length){
+            System.out.println("Process is successful, num: "+ num + ", buyers.length is: " + arr.length);
+            int newNum = (arr.length*3)/2;
             Object[] newBuyers = new Object[newNum];
-            System.arraycopy(buyers, 0, newBuyers, 0, buyers.length);
-            buyers=newBuyers;
+            System.arraycopy(arr, 0, newBuyers, 0, arr.length);
+            arr=newBuyers;
         }
     }
     @Override
     public String toString(){
         StringJoiner result1 = new StringJoiner(", ");
-        for(int i =0; i<numberOfBuyers; i++){
-            result1.add(buyers[i].toString());
+        for(int i =0; i<number; i++){
+            result1.add(arr[i].toString());
         }
         return "["+ result1 +"]";
     }
     //очищає колекцію:
     void clear(){
-        Arrays.fill(buyers, null);
+        Arrays.fill(arr, null);
         System.out.println("After clearing Array:");
-        buyers = new Object[buyers.length];
-        System.out.println(Arrays.toString(buyers));
+        arr = new Object[arr.length];
+        System.out.println(Arrays.toString(arr));
     }
     //повертає розмір колекції:
-    void size(){
-        System.out.println(buyers.length);
+    public int size(){
+        return arr.length;
     }
     //повертає перший елемент з черги:
-    void peek(){
-        System.out.println(buyers[0]);
+    public Object peek(){
+        return arr[0];
     }
     //повертає перший елемент з черги і видаляє його з колекції:
-    void poll(){
-        System.out.println(buyers[0]);
+    public Object poll(){
         int b=0;
-        Object[] a = new Object[buyers.length-1];
-         for(int i = 1; i<buyers.length; i++){
-            a[b]=buyers[i];
+        Object[] a = new Object[arr.length-1];
+         for(int i = 1; i<arr.length; i++){
+            a[b]=arr[i];
             b++;
         }
-        System.out.println(Arrays.toString(a));
-        System.out.println("size after poll: " + a.length);
-    }
+   return "First element: " + arr[0]+ "afterPoll: "+Arrays.toString(a);}
 }
